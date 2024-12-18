41a9e42f0e1ec037fe4cae1db1fb891cfe3cd5da
iiiiins <83681280+iiiiins@users.noreply.github.com>
2024-12-18 17:20:40 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './Quake_logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello from Render!
+            Quake Rankings
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://youtu.be/GZV1-pEY3po"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            View Rankings!
           </a>
         </header>
       </div>
